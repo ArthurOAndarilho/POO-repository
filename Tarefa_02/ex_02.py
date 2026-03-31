04780a40316c1fe5640d6b909bf3ca26d38e884b
ArthurOAndarilho <arthur.farias@escolar.ifrn.edu.br>
2026-03-31 18:59:19 +0000

    corrigindo denovo

--- Tarefa_02/ex_02.py
+++ Tarefa_02/ex_02.py
@@ -1,18 +1,14 @@
 class Pais:
     def __init__(self):
         self.nome = ""
-        self.população = 1
+        self.populacao = 1
         self.area = 1
 
     def calc_DD(self):
-        return self.população / self.area
+        return self.populacao / self.area
     
-lista = []
-for k in range[10]:
-
-    p = Pais()
-    p.nome = input("Nome: ")
-    p.populacao = int(input("População: "))
-    p.area = float(input("Área: "))
-
-    lista.append(p)
+p = Pais()
+p.nome = input("Nome: ")
+p.populacao = int(input("População: "))
+p.area = float(input("Área: "))
+print(p.calc_DD())
