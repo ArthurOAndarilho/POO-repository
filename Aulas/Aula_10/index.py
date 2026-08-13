1bbacd0a71875ed4eacc7071b752ee703a10eeeb
ArthurOAndarilho <arthur.farias@escolar.ifrn.edu.br>
2026-08-13 17:30:10 +0000

    concluindo tarefas

---
+++ Aulas/Aula_10/index.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from Aulas.Aula_10.retanguloUI import RetanguloUI
+
+RetanguloUI.main()
+
+#st.header("POO em Python com Streamlit")
+
+#if st.button("Clique Aqui"):
+#    st.write("Bem-vindo(a) ao Streamlit")
---
+++ Listas/Lista_08/paciente.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+class Paciente:
+    def __init__(self, nome, cpf, fone, nascimento):
+        self.__nome = nome
+        self.__cpf = cpf
+        self.__fone = fone
+        self.__nascimento = nascimento
+    def __str__(self):
+        return f"{self.__nome} - {self.__cpf} - {self.__fone} - {self.__nascimento.strftime('%d/%m/%Y')}"
+    def idade(self):
+        x = datetime.now() - self.__nascimento
+        dias = x.days
+        anos = dias // 365
+        meses = dias % 365 // 30
+        return f"{anos} ano(s) e {meses} mes(es)"
+
+x = Paciente("Nome", "123", "456", datetime(2010, 2, 23))
+print(x)
+print(x.idade())
